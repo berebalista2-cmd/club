4638f2533d88cf2f1cd059c5cb30c1b97ed86085
DaniloFeling <dhf456@gmail.com>
2025-10-10 15:09:10 -0300

    corregido database y app.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname'     => 'localhost',
         'username'     => 'root',
         'password'     => '',
-        'database'     => 'gestion',
+        'database'     => 'club',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
