5e9ea606fc2865ba93822ded6ed2f5d6e74d9926
berebalista2-cmd <berenicebalista2@gmail.com>
2025-10-26 16:39:41 -0300

    Update Routes.php
    eliminacion de rutas clientes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,13 +11,8 @@ $routes->get('/panel/index', 'Panel::index');
 $routes->get('/panel/nuevo', 'Panel::nuevo');
 $routes->get('/paises', 'Paises::index');
 $routes->get('/paises/listar', 'Paises::listar');
-$routes->get('/clientes', 'Clientes::index');
 $routes->get('/proveedores', 'Proveedores::index');
-$routes->get('/clientes/nuevo', 'Clientes::nuevo');
-$routes->post('/clientes/guardar', 'Clientes::guardar');
-$routes->get('/clientes/borrar/(:num)', 'Clientes::borrar/$1');
-$routes->get('/clientes/editar/(:num)', 'Clientes::editar/$1');
-$routes->post('/clientes/actualizar/(:num)', 'Clientes::actualizar/$1');
+
 
 //zonas
 $routes->get('/zonas/nuevo', 'Zonas::nuevo');
