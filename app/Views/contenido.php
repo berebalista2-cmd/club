32a46915a282ea634e37eee4de8b84cce1700ff1
berebalista2-cmd <berenicebalista2@gmail.com>
2025-10-26 16:35:43 -0300

    Update contenido.php
    eliminacion de referencia a clientes

--- app/Views/contenido.php
+++ app/Views/contenido.php
@@ -1,10 +1,1 @@
 <h3>Soy el contenido</h3>
-
-<?php
-    foreach ($clientes as $cliente){
-        echo '<p class="cliente">'. $cliente['denominacion']. '</p>';
-        echo '<br>';
-    }
-
-   // echo base_url(); lo trajimos de esta forma porque es un metodo, igual dudo tener que hacerlo de nuevo
-?>
