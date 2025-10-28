b7e1e11a6c1ae8bda69766b11c5dd69e297e9aeb
Diego Mansilla <diiego.mansilla@gmail.com>
2025-10-28 11:40:14 -0300

    Correcciones en controlador y vistas de Recaudadores

--- app/Views/recaudadores/editar.php
+++ app/Views/recaudadores/editar.php
@@ -1,21 +1,21 @@
 <form action="
-<?php 
+<?php
 echo base_url();
 
-?>public/recaudadores/actualizar/<?php echo $recaudador[0]['id'];?>
+?>public/recaudadores/actualizar/<?php echo $recaudador[0]['id']; ?>
 " method="post">
 
     <label class="form-control" for="nombre">Nombre</label>
-    <input class="form-control" type="text" name="nombre" id="nombre" 
-    placeholder="Ingrese un nombre" value="<?php echo $recaudador[0]['nombre'];?>">
+    <input class="form-control" type="text" name="nombre" id="nombre"
+        placeholder="Ingrese un nombre" value="<?php echo $recaudador[0]['nombre']; ?>">
 
     <label class="form-control" for="apellido">Apellido</label>
-    <input class="form-control" type="text" name="apellido" id="apellido" 
-    placeholder="Ingrese un apellido" value="<?php echo $recauddador[0]['apellido'];?>">
+    <input class="form-control" type="text" name="apellido" id="apellido"
+        placeholder="Ingrese un apellido" value="<?php echo $recaudador[0]['apellido']; ?>">
 
-    <label class="form-control" for="dni">D.N.I.</label>
-    <input class="form-control" type="text" name="dni" id="dni" 
-    placeholder="Ingrese D.N.I." value="<?php echo $recaudador[0]['dni'];?>">
+    <label class="form-control" for="dni">D.N.I</label>
+    <input class="form-control" type="text" name="dni" id="dni"
+        placeholder="Ingrese D.N.I" value="<?php echo $recaudador[0]['dni']; ?>">
 
     <input class="btn btn-success" type="submit" value="Guardar">
     <a class="btn btn-danger" href="
