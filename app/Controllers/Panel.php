b61785b1ef505755abaaca36f3c55bd79ae72ff1
DaniloFeling <dhf456@gmail.com>
2025-11-03 15:29:33 -0300

    cambios en el panel

--- app/Controllers/Panel.php
+++ app/Controllers/Panel.php
@@ -2,43 +2,68 @@
 
 namespace App\Controllers;
 
+use App\Models\UsuariosModel; 
+use App\Models\SociosModel;
+use App\Models\RecaudadoresModel;
+use App\Models\ZonasModel;
 class Panel extends BaseController
 {
-    
-    public function __construct()
-    { //La función constructora
-
+    protected $usuarios; 
+    protected $socios;
+    protected $recaudadores;
+    protected $zonas;
+    public function __construct() { //La función constructora
+        
+        $this->usuarios = new UsuariosModel();
+        $this->socios = new SociosModel();
+        $this->recaudadores = new RecaudadoresModel();
+        $this->zonas = new ZonasModel();
+        
         $this->sesion = session();
+
+           
     }
+
     public function index()
     {
-        if (!isset($this->sesion->id_usuario)) {
-            return redirect()->to(base_url() . 'public/login');
+       if(!isset($this->sesion->id_usuario)){
+        return redirect()->to(base_url() .'public/login');
         }
 
-        $context = [
-            'titulo' => 'Tablero',
-        ];
+        //Esto seria una consulta,  "Select * from clientes"
+        $usuarios = $this->usuarios->findAll();
+        $socios = $this->socios->findAll();
+        $recaudadores = $this->recaudadores->findAll();
+        $zonas = $this->zonas->findAll();
+        
+        $totalUsuarios = $this->usuarios->countAll();
+        $totalSocios = $this->socios->countAll();
+        $totalRecaudadores = $this->recaudadores->countAll();
+        $totalZonas = $this->zonas->countAll();
+        
+        
+        $context = ['titulo' => 'Tablero', 'totalSocios' => $totalSocios,
+                     'totalUsuarios' => $totalUsuarios,
+                    'totalRecaudadores' => $totalRecaudadores,
+                    'totalZonas' => $totalZonas];
+       
 
         echo view('panel/header', $context);
         echo view('panel/contenido');
         echo view('panel/footer');
-        // echo view('inicio', $contex);
-        //echo view('contenido', $contex);
-        //echo view('fin');
-
+ 
     }
 
     public function nuevo()
     {
-        if (!isset($this->sesion->id_usuario)) {
-            return redirect()->to(base_url() . 'public/login');
+        if(!isset($this->sesion->id_usuario)){
+        return redirect()->to(base_url() .'public/login');
         }
-        $context['titulo'] = "Panel"; //los valores de contexto pueden ser cualquier tipo de dato, siempre tiene que tener
-        $context['subtitulo'] = "Este es el panel de control";
-        $context['pagname'] = "Panel de control";
-        echo view('inicio', $context);
-        echo view('nuevo', $context); //se envia la variable de contexto a esta vista
+        $contex['titulo'] = "Panel"; //los valores de contexto pueden ser cualquier tipo de dato, siempre tiene que tener
+        $contex['subtitulo'] = "Este es el panel de control";
+        $contex['pagname'] = "Panel de control";
+        echo view('inicio', $contex);
+        echo view('nuevo', $contex); //se envia la variable de contexto a esta vista
         echo view('fin');
     }
 }
